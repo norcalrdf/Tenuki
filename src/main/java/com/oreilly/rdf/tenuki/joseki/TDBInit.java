436b6de66314aad87952d246a3154d4f2adfd380
Gavin Carothers <gavin@oreilly.com>
2009-06-26 10:39:19 -0700

    Working Joseki integration

---
+++ src/main/java/com/oreilly/rdf/tenuki/joseki/TDBInit.java
@@ -0,0 +1,21 @@
+package com.oreilly.rdf.tenuki.joseki;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.joseki.ServerInitialization;
+
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.tdb.TDB;
+import com.oreilly.rdf.tenuki.restlet.ChangesetResource;
+
+public class TDBInit implements ServerInitialization {
+	private Log log = LogFactory.getLog(TDBInit.class);
+
+	
+	@Override
+	public void init(Resource service, Resource implementation) {
+		log.info("Setting TDB unionDefaultGraph");
+		TDB.getContext().set(TDB.symUnionDefaultGraph, true);
+	}
+
+}
