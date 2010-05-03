2da85e6e945243b6106069a58c49a2a9e0b7ca13
Gavin Carothers <gavin@oreilly.com>
2010-05-03 12:18:30 -0700

    Write Jena Models using JAX-RS
    --HG--
    branch : jax-rs

---
+++ src/main/java/com/oreilly/com/rdf/tenuki/jaxrs/ModelWriter.java
@@ -0,0 +1,55 @@
+package com.oreilly.com.rdf.tenuki.jaxrs;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyWriter;
+import javax.ws.rs.ext.Provider;
+
+import com.hp.hpl.jena.rdf.model.Model;
+
+@Provider
+@Produces("application/rdf+xml,text/turtle,text/plain,text/rdf+n3")
+public class ModelWriter implements MessageBodyWriter<Model> {
+
+	@Override
+	public long getSize(Model t, Class<?> type, Type genericType,
+			Annotation[] annotations, MediaType mediaType) {
+		return -1;
+	}
+
+	@Override
+	public boolean isWriteable(Class<?> type, Type genericType,
+			Annotation[] annotations, MediaType mediaType) {
+		return type.isInstance(Model.class);
+	}
+
+	@Override
+	public void writeTo(Model model, Class<?> type, Type genericType,
+			Annotation[] annotations, MediaType mediaType,
+			MultivaluedMap<String, Object> httpHeaders,
+			OutputStream entityStream) throws IOException,
+			WebApplicationException {
+		String lang = null;
+		if (mediaType.isCompatible(MediaType.valueOf("application/rdf+xml"))) {
+			lang = "RDF/XML";
+		}
+		if (mediaType.isCompatible(MediaType.valueOf("text/turtle"))) {
+			lang = "TTL";
+		}
+		if (mediaType.isCompatible(MediaType.valueOf("text/rdf+n3"))) {
+			lang = "N3";
+		}
+		if (mediaType.isCompatible(MediaType.valueOf("text/plain"))) {
+			lang = "N-TRIPLE";
+		}
+		model.write(entityStream, lang);
+	}
+
+}
