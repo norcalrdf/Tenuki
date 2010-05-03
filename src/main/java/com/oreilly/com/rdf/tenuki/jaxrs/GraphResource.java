df674b721495acbe97fb1e7695e13b0472b4efe0
Gavin Carothers <gavin@oreilly.com>
2010-05-03 12:54:06 -0700

    Starting first JAX-RS Resource.
    --HG--
    branch : jax-rs

---
+++ src/main/java/com/oreilly/com/rdf/tenuki/jaxrs/GraphResource.java
@@ -0,0 +1,33 @@
+package com.oreilly.com.rdf.tenuki.jaxrs;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import com.hp.hpl.jena.query.Dataset;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.sparql.core.DatasetImpl;
+import com.hp.hpl.jena.tdb.store.DatasetGraphTDB;
+import com.talis.tdb.bdb.BDBinstance;
+import com.talis.tdb.bdb.SetupBDB;
+
+@Path("/graphs/")
+public class GraphResource {
+	
+	private Dataset dataset;
+
+	public GraphResource() {
+		String location = "tdb_bdb";
+		BDBinstance bdb = new BDBinstance(location);
+		DatasetGraphTDB dsg = SetupBDB.buildDataset(bdb);
+		this.dataset = new DatasetImpl(dsg);	}
+	
+	@Path("{graphUri}")
+	@Produces("application/rdf+xml")
+	@GET
+	public Model getGraph(@PathParam("graphUri") String graphUri) {
+		return dataset.getNamedModel(graphUri);
+	}
+
+}
