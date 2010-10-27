6d06c811f2536de64f938a7304155e62214aceca
Gavin Carothers <gavin@oreilly.com>
2010-10-26 17:15:04 -0700

    Load port from config file other wise default or override with commandline option

--- src/main/java/com/oreilly/rdf/tenuki/Tenuki.java
+++ src/main/java/com/oreilly/rdf/tenuki/Tenuki.java
@@ -7,6 +7,7 @@ import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.cli.PosixParser;
+import org.apache.commons.configuration.HierarchicalINIConfiguration;
 import org.apache.commons.dbcp.BasicDataSource;
 import org.eclipse.jetty.plus.jndi.Resource;
 import org.eclipse.jetty.server.Server;
@@ -41,7 +42,14 @@ public class Tenuki {
 			}
 			
 			System.err.println("Starting Tenuki...");
-
+			Integer port = 7070;
+			HierarchicalINIConfiguration config = null;
+			if (line.getArgList().size() > 0) {
+				String configFilePath = line.getArgs()[0];
+				config = new HierarchicalINIConfiguration(configFilePath);
+				port = config.getInt("server.port", 7070);
+			}
+			
 			BasicDataSource dataSource = new BasicDataSource();
 			dataSource.setDriverClassName("org.postgresql.Driver");
 			dataSource.setUrl("jdbc:postgresql:sdb");
@@ -50,8 +58,8 @@ public class Tenuki {
 
 			StoreDesc storeDesc = new StoreDesc("layout2/index", "postgresql");
 
-			Integer port = Integer
-					.parseInt(line.getOptionValue("port", "7070"));
+			port = Integer
+					.parseInt(line.getOptionValue("port", port.toString()));
 			
 			System.err.println("... configuration complete ...");
 			
