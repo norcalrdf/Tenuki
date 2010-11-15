8e0931b62fe82014968bb4fab3c45316f7ceee74
Gavin Carothers <gavin@carothers.name>
2010-11-14 20:16:22 -0800

    Create a context even if we aren't using it just at the moment

--- src/main/java/com/oreilly/rdf/tenuki/jaxrs/UIResource.java
+++ src/main/java/com/oreilly/rdf/tenuki/jaxrs/UIResource.java
@@ -3,6 +3,9 @@ package com.oreilly.rdf.tenuki.jaxrs;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.context.Context;
+
 import com.sun.jersey.api.view.Viewable;
 
 @Path("/")
@@ -10,6 +13,7 @@ public class UIResource {
 
 	@GET
 	public Viewable landingPage() {
-		return new Viewable("/templates/test.vm");
+		Context c = new VelocityContext();
+		return new Viewable("/templates/test.vm", c);
 	}
 }
