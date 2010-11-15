003b9a8c2798428cd768cd8d4ed779c176cd84e3
Gavin Carothers <gavin@carothers.name>
2010-11-14 16:23:09 -0800

    Left over import

--- src/test/java/com/oreilly/rdf/tenuki/api/APITest.java
+++ src/test/java/com/oreilly/rdf/tenuki/api/APITest.java
@@ -1,7 +1,6 @@
 package com.oreilly.rdf.tenuki.api;
 
 import com.sun.jersey.test.framework.JerseyTest;
-import com.sun.jersey.test.framework.WebAppDescriptor;
 
 public abstract class APITest extends JerseyTest {
 	public APITest() {
